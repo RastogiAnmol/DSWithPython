378d31e89e5970aebb3a065c7d72e8188279b894
Anmol Rastogi <anmolamity@gmail.com>
2019-07-15 18:40:29 +0530

    Basic LinkedList operations

--- SinglyLinkedList.py
+++ SinglyLinkedList.py
@@ -1,4 +1,5 @@
-# A simple Python program for traversal of a linked list
+# A complete working Python program to demonstrate all
+# insertion methods of linked list
 
 # Node class
 
@@ -18,11 +19,67 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    # This function prints contents of linked list
-    # starting from head
-    def print_list(self):
+    # Function to insert a new node at the beginning
+    def push(self, new_data):
+
+        # 1 & 2: Allocate the Node &
+        # Put in the data
+        new_node = Node(new_data)
+
+        # 3. Make next of new Node as head
+        new_node.next = self.head
+
+        # 4. Move the head to point to new Node
+        self.head = new_node
+
+    # This function is in LinkedList class. Inserts a
+    # new node after the given prev_node. This method is
+    # defined inside LinkedList class shown above */
+    def insertAfter(self, prev_node, new_data):
+
+        # 1. check if the given prev_node exists
+        if prev_node is None:
+            print("The given previous node must be present in LinkedList.")
+            return
+
+        # 2. create new node &
+        # Put in the data
+        new_node = Node(new_data)
+
+        # 4. Make next of new Node as next of prev_node
+        new_node.next = prev_node.next
+
+        # 5. make next of prev_node as new_node
+        prev_node.next = new_node
+
+    # This function is defined in Linked List class
+    # Appends a new node at the end. This method is
+    # defined inside LinkedList class shown above */
+    def append(self, new_data):
+
+        # 1. Create a new node
+        # 2. Put in the data
+        # 3. Set next as None
+        new_node = Node(new_data)
+
+        # 4. If the Linked List is empty, then make the
+        # new node as head
+        if self.head is None:
+            self.head = new_node
+            return
+
+        # 5. Else traverse till the last node
+        last = self.head
+        while (last.next):
+            last = last.next
+
+        # 6. Change the next of last node
+        last.next = new_node
+
+    # Utility function to print the linked list
+    def printList(self):
         temp = self.head
-        while temp:
+        while (temp):
             print(temp.data)
             temp = temp.next
 
@@ -31,12 +88,12 @@ class LinkedList:
 if __name__ == '__main__':
     # Start with the empty list
     llist = LinkedList()
+    llist.push(5)
+    llist.push(4)
+    llist.push(3)
+    llist.push(1)
+    llist.append(6)
+    llist.insertAfter(llist.head, 2)
 
-    llist.head = Node(1)
-    second = Node(2)
-    third = Node(3)
-
-    llist.head.next = second  # Link first node with second
-    second.next = third  # Link second node with the third node
-
-    llist.print_list()
+    print('Created linked list is:')
+    llist.printList()
